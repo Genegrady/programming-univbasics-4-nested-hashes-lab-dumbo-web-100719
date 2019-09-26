2f7da11d129d9c9581e1f44d4ac06c56f80918de
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-26 16:43:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,7 +63,10 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -100,7 +103,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+    programmer_hash[:alan_kay] = {
+       :known_for => "GUI"
+     }
      
      
 end
